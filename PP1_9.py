c01d87ce6731c8a0b5ea47e30c0f9afa53545901
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-10-01 13:17:52 +0000

    dwa

--- PP1_9.py
+++ PP1_9.py
@@ -2,23 +2,34 @@
 
 def q1(): 
   #Write Assignment code here
-
+  print('"Hello World"')
 
 def q2(): 
   #Write Assignment code here
-
+  a = input("Input a word:")
+  print(a.lower())
+  print(a.upper())
 
 def q3(): 
   #Write Assignment code here
-
-
+  a = input("Input a word that is at least 5 letters long: ")
+  if len(a) >= 5:
+        print(a[1:4])
+  else:
+        print("The word is not long enough.")
+  
 def q4(): 
   #Write Assignment code here
-
-
+  a = input("Input a word:")
+  index = a.find('o')
+  if index != -1:
+        print(f"The first occurrence of 'o' is at index {index}.")
+  else:
+        print("The letter 'o' was not found.")
 def q5(): 
   #Write Assignment code here
-
+  a = input("Inout a word:")
+  print(len(a))
 
 
 #Do not alter the following code
